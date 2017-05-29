a0602c71adc7fe949b802f331f9d4b3442e27ffb
Haoguang (John) Yang <yhg14@mails.tsinghua.edu.cn>
2017-05-29 15:31:55 +0800

    updated HW3, crashing bug when not utilizing all nodes in COMM_WORLD is fixed

--- HW3/jacobi.cpp
+++ HW3/jacobi.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <algorithm>
+#include <numeric>
 #include <math.h>
 #include <mpi.h>
 #include <omp.h>
@@ -106,7 +107,7 @@ int main(int argc, char* argv[]) {
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
-    if (rank==0) printf("Size:%dx%dx%d, # of Steps: %d, # of procs: %d, # of threads per proc: %d\n", \
+    if (rank==0) printf("Size:%dx%dx%d, # of Steps: %d, max # of Procs: %d, # of Threads per Proc: %d\n", \
         maxm, maxn, maxp, loop, size, omp_get_num_threads());
 
     /* xlocal[][0] is lower ghostpoints, xlocal[][maxn+2] is upper */
@@ -168,10 +169,8 @@ int main(int argc, char* argv[]) {
 	int MPI_Grid2[MPI_GS2+1];
 	int MPI_Grid3[MPI_GS3+1];
 
-	if (MPI_GS1*MPI_GS2*MPI_GS3<size) {
-		printf("Improper Cluster Size! Please Utilize More Nodes.\n");
-		return -1;
-	}
+	if (rank == 0) printf("Actually utilizing %d * %d * %d = %d procs.\n", MPI_GS1, MPI_GS2, MPI_GS3, MPI_GS1*MPI_GS2*MPI_GS3);
+	
 	for (int i=0; i<std::max(std::max(MPI_GS1,MPI_GS2),MPI_GS3); i++){
 		int tmp = unit_length*i;
 		if (i<MPI_GS1)	MPI_Grid1[i] = tmp;
@@ -186,7 +185,9 @@ int main(int argc, char* argv[]) {
 	
 	int ind1 = rank%MPI_GS1;
 	int ind2 = (rank/MPI_GS1)%MPI_GS2;
-	int ind3 = rank/(MPI_GS1*MPI_GS2);
+	int ind3 = (rank/(MPI_GS1*MPI_GS2))%MPI_GS3;
+	//Actually, for floor(rank/(MPI_GS1*MPI_GS2)/MPI_GS3) > 0, these nodes will do redundent calculations.
+	bool isActive = !(bool)floor(rank/(MPI_GS1*MPI_GS2)/MPI_GS3);
 	
 	i0 = 1;
 	i1 = MPI_Grid1[ind1+1]-MPI_Grid1[ind1];
@@ -266,7 +267,7 @@ int main(int argc, char* argv[]) {
 	MPI_Datatype slicei;
 	int subsizei[3] = {1, xsize[1]-2, xsize[2]-2};
 	
-	if (ind1 < MPI_GS1 - 1){
+	if (isActive && ind1 < MPI_GS1 - 1){
 	    //MPI_Send( &(xlocal[i1]), (j1+2)*(k1+2), MPI_DOUBLE, rank + 1, 0, 
 		//      MPI_COMM_WORLD );
 		int starts[3] = {i1,1,1};
@@ -276,7 +277,7 @@ int main(int argc, char* argv[]) {
 		      MPI_COMM_WORLD );
 		//printf( "Flag1.1\n" );
 	}
-	if (ind1 > 0){
+	if (isActive && ind1 > 0){
 	    //MPI_Recv( &(xlocal[0]), (j1+2)*(k1+2), MPI_DOUBLE, rank - 1, 0, 
 		//      MPI_COMM_WORLD, &status );
 		int starts[3] = {0,1,1};
@@ -287,7 +288,7 @@ int main(int argc, char* argv[]) {
 		//printf( "Flag1.2\n" );
 	}
 	/* Send down unless I'm at the bottom */
-	if (ind1 > 0){
+	if (isActive && ind1 > 0){
 	    //MPI_Send( &(xlocal[1]), (j1+2)*(k1+2), MPI_DOUBLE, rank - 1, 1, 
 		//      MPI_COMM_WORLD );
 		int starts[3] = {1,1,1};
@@ -297,7 +298,7 @@ int main(int argc, char* argv[]) {
 		      MPI_COMM_WORLD );
 		//printf( "Flag1.3\n" );
 	}
-	if (ind1 < MPI_GS1 - 1){
+	if (isActive && ind1 < MPI_GS1 - 1){
 	    //MPI_Recv( &(xlocal[i1+1]), (j1+2)*(k1+2), MPI_DOUBLE, rank + 1, 1, 
 		//      MPI_COMM_WORLD, &status );
 		int starts[3] = {i1+1,1,1};
@@ -320,7 +321,7 @@ int main(int argc, char* argv[]) {
 	//MPI_Type_create_subarray(3, xsize, subsizej, starts, MPI_ORDER_C, MPI_DOUBLE, &slicej);
 	//MPI_Type_commit(&slicej);
 	//MPI_Barrier(MPI_COMM_WORLD);
-	if (ind2 < MPI_GS2 - 1){
+	if (isActive && ind2 < MPI_GS2 - 1){
 		int starts[3] = {1,j1,1};
 		MPI_Type_create_subarray(3, xsize, subsizej, starts, MPI_ORDER_C, MPI_DOUBLE, &slicej);
 		MPI_Type_commit(&slicej);
@@ -328,7 +329,7 @@ int main(int argc, char* argv[]) {
 		      MPI_COMM_WORLD );
 		//printf( "Flag2.1\n" );
 	}
-	if (ind2 > 0){
+	if (isActive && ind2 > 0){
 		int starts[3] = {1,0,1};
 		MPI_Type_create_subarray(3, xsize, subsizej, starts, MPI_ORDER_C, MPI_DOUBLE, &slicej);
 		MPI_Type_commit(&slicej);
@@ -337,7 +338,7 @@ int main(int argc, char* argv[]) {
 		//printf( "Flag2.2\n" );
 	}
 	/* Send down unless I'm at the bottom */
-	if (ind2 > 0){
+	if (isActive && ind2 > 0){
 		int starts[3] = {1,1,1};
 		MPI_Type_create_subarray(3, xsize, subsizej, starts, MPI_ORDER_C, MPI_DOUBLE, &slicej);
 		MPI_Type_commit(&slicej);
@@ -345,7 +346,7 @@ int main(int argc, char* argv[]) {
 		      MPI_COMM_WORLD );
 		//printf( "Flag2.3\n" );
 	}
-	if (ind2 < MPI_GS2 - 1){
+	if (isActive && ind2 < MPI_GS2 - 1){
 		int starts[3] = {1,j1+1,1};
 		MPI_Type_create_subarray(3, xsize, subsizej, starts, MPI_ORDER_C, MPI_DOUBLE, &slicej);
 		MPI_Type_commit(&slicej);
@@ -366,14 +367,14 @@ int main(int argc, char* argv[]) {
 	//MPI_Type_create_subarray(3, xsize, subsizek, starts, MPI_ORDER_C, MPI_DOUBLE, &slicek);
 	//MPI_Type_commit(&slicek);
 	//MPI_Barrier(MPI_COMM_WORLD);
-	if (ind3 < MPI_GS3 - 1){
+	if (isActive && ind3 < MPI_GS3 - 1){
 		int starts[3] = {1,1,k1};
 		MPI_Type_create_subarray(3, xsize, subsizek, starts, MPI_ORDER_C, MPI_DOUBLE, &slicek);
 		MPI_Type_commit(&slicek);
 	    MPI_Send( &xlocal[0][0][0], 1, slicek, rank + MPI_GS1*MPI_GS2, 4, 
 		      MPI_COMM_WORLD );
 	}
-	if (ind3 > 0){
+	if (isActive && ind3 > 0){
 		int starts[3] = {1,1,0};
 		MPI_Type_create_subarray(3, xsize, subsizek, starts, MPI_ORDER_C, MPI_DOUBLE, &slicek);
 		MPI_Type_commit(&slicek);
@@ -381,14 +382,14 @@ int main(int argc, char* argv[]) {
 		      MPI_COMM_WORLD, &status );
 	}
 	/* Send down unless I'm at the bottom */
-	if (ind3 > 0){
+	if (isActive && ind3 > 0){
 		int starts[3] = {1,1,1};
 		MPI_Type_create_subarray(3, xsize, subsizek, starts, MPI_ORDER_C, MPI_DOUBLE, &slicek);
 		MPI_Type_commit(&slicek);
 	    MPI_Send( &xlocal[0][0][0], 1, slicek, rank - MPI_GS1*MPI_GS2, 5, 
 		      MPI_COMM_WORLD );
 	}
-	if (ind3 < MPI_GS3 - 1){
+	if (isActive && ind3 < MPI_GS3 - 1){
 		int starts[3] = {1,1,k1+1};
 		MPI_Type_create_subarray(3, xsize, subsizek, starts, MPI_ORDER_C, MPI_DOUBLE, &slicek);
 		MPI_Type_commit(&slicek);
@@ -397,7 +398,7 @@ int main(int argc, char* argv[]) {
 	}
 	//printf( "Communication of Dim3 Done\n" );
 	/* Compute new values (but not on boundary) */
-	if (rank==0) printf( "Communication of timestep %d is done...\n", t );
+	if (isActive && rank==0) printf( "Communication of timestep %d is done...\n", t );
 	//diffnorm = 0.0;
 	//MPI_Barrier(MPI_COMM_WORLD);                          //All Data is Updated
 	#pragma omp parallel for schedule(guided)
@@ -448,12 +449,23 @@ int main(int argc, char* argv[]) {
 	double surface = 2*(maxm*maxn+maxn*maxp+maxm*maxp);
 	double diff = fabs(sum-sum2)/surface/loop;
 	//printf( "Average Difference = %f\n", diff );
-	if (diff < 0.001 && surface>1000) printf( "Result is Correct.\n" );
-	else if (surface<=1000) printf("Mesh size is too small for randomness to damp down.\n");
-	else printf("Result is Wrong.\n");
+	bool *Ans = NULL;
+	bool isCorrect = false;
+	if (rank == 0){
+		Ans = (bool*)malloc(size*sizeof(bool));
+	}
+	if ((diff < 0.001 && surface>1000) || !isActive) isCorrect = true;
+	MPI_Gather(&isCorrect, 1, MPI_C_BOOL, Ans, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+	
+	if (rank == 0){
+		if (std::accumulate(Ans, Ans+size, 0)==size) printf( "Result is Correct.\n" );
+		else if (surface<=1000) printf("Mesh size is too small for randomness to damp down.\n");
+		else printf("Result is Wrong.\n");
+	}
 	//Don't forget to free array!!!
 	free(xlocal);
 	free(xnew);
+	free(Ans);
 	MPI_Finalize();
     return 0;
 }
